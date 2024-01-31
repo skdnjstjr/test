cad29665a1ae64bb87fc95d9e59cb8465c1c2f56
skdnjstjr <skdnjstjr0121@naver.com>
2024-01-31 14:31:05 +0900

    [test] 프로그램 test

---
+++ src/main/java/com/ohgiraffers/test/TestApplication.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.test;
+
+public class TestApplication {
+
+    public static void main(String[] args) {
+        System.out.println("저는 개발자 입니다. ");}
+
+}
+/* 여기는
+여러줄
+주석입니
+ */
+/*dfsdkf*/
+
